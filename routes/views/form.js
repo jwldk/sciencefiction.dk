6ebd39e477bac1d1fa70c3f48f735c9fc10f17d5
Jeppe Larsen <jwl@sysctl.dk>
2016-12-02 15:27:08 +0100

    Add missing view

---
+++ routes/views/form.js
@@ -0,0 +1,37 @@
+import keystone from 'keystone';
+const Form = keystone.list('Form');
+const FormSubmit = keystone.list('Formsubmit');
+
+exports = module.exports = (req, res) => {
+
+  const view = new keystone.View(req, res);
+  const locals = res.locals;
+
+  // locals.section is used to set the currently selected
+  // item in the header navigation.
+  locals.section = 'articles';
+
+  view.on('init', (next) => {
+    Form.model.findOne()
+      .where('key', req.params.key)
+      .exec((err, form) => {
+        if(!form) {
+          return res.status(404).render('errors/404');
+        }
+        locals.form = form;
+        next();
+      });
+  });
+
+  view.on('post', (next) => {
+    var submit = new FormSubmit.model();
+    var updater = submit.getUpdateHandler(req);
+
+    updater.process(req.body, {}, (err) => {
+      locals.message = locals.form.successMessage;
+      next();
+    });
+  });
+  view.render('form');
+};
+
