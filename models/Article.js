c85c7e8fd3120ed59f48314130761d4c2e1bf25c
Jeppe Larsen <jwl@sysctl.dk>
2017-02-27 14:40:18 +0100

    Added support for file uploads to Article model

--- models/Article.js
+++ models/Article.js
@@ -7,6 +7,19 @@ const Article = new keystone.List('Article', {
   autokey: { from: 'title', path: 'key', unique: true },
   map: { name: 'title'}
 });
+const articleStorage = new keystone.Storage({
+  adapter: keystone.Storage.Adapters.FS,
+  fs: {
+    path: keystone.expandPath('./public/files/articles'),
+    publicPath: '/files/articles/',
+    generateFilename: function(file) { return file.originalname },
+    whenExists: 'overwrite'
+  },
+  schema: {
+    url: true,
+    originalname: true
+  }
+});
 
 Article.add({
   createdAt: { type: Types.Date, default: Date.now },
@@ -16,6 +29,7 @@ Article.add({
     extended: { type: Types.Html, wysiwyg: true, height: 400 }
   },
   frontpage: { type: Types.Boolean, default: false },
+  file: { type: Types.File, storage: articleStorage },
   articletype: { type: Types.Select, options: 'static, nyhed, generalforsamling' }
 });
 
